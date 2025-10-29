bab23c418c0a967865eac5df56b3c62f4b3016dc
mzHap <mojcazamuda12@gmail.com>
2025-10-29 21:55:00 +0100

    Add timer component to QuizHost page

--- src/pages/QuizHost/QuizHost.tsx
+++ src/pages/QuizHost/QuizHost.tsx
@@ -1,27 +1,34 @@
-import { Flex, Image } from "antd"
-import styles from "./QuizHost.module.css"
-import jabuka from './apple_temp.png'
-import { useState, useEffect } from "react"
+import { Flex, Image } from "antd";
+import styles from "./QuizHost.module.css";
+import jabuka from './apple_temp.png';
+import { useState, useEffect } from "react";
+import Timer from "../../components/Timer/Timer";
 
+export default function QuizHost() {
+  const [rotation, setRotation] = useState(0);
 
-export default function QuizHost(){
+  useEffect(() => {
+    const randomRotation = Math.floor(Math.random() * 21) - 10; 
+    setRotation(randomRotation);
+  }, []);
 
-    const [rotation, setRotation] = useState(0);
+  return (
+    <>
+      <div className={styles.timerWrapper}>
+        <Timer totalSeconds={15} />
+      </div>
 
-    useEffect(() => {
-        const randomRotation = Math.floor(Math.random() * 21) - 10; 
-        setRotation(randomRotation);
-    }, []);
-
-
-
-    return(
-    <Flex className={styles.question}>
-
-        <Image className={styles.image} src={jabuka} preview={false} style={{ transform: `rotate(${rotation}deg)` }}> {/*TEMPORARY DOKLER NI POVEZANO*/}
-        </Image>
-        <h1 style={{margin: '0px', textAlign: 'center'}}>
-            ADD TEXT HERE
+      <Flex className={styles.question}>
+        <Image
+          className={styles.image}
+          src={jabuka}
+          preview={false}
+          style={{ transform: `rotate(${rotation}deg)`, marginBottom: '10px' }}
+        />
+        <h1 style={{ margin: '0px', textAlign: 'center' }}>
+          ADD TEXT HERE
         </h1>
-    </Flex>)
+      </Flex>
+    </>
+  );
 }
