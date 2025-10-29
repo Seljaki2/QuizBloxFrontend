4ca340ea92b7f942b03def0be5d7ecd630c413e4
mzHap <mojcazamuda12@gmail.com>
2025-10-29 18:47:48 +0100

    Add QuizHost page with custom layout and PlusJakartaSans font, missing background and timer

---
+++ src/pages/QuizHost/QuizHost.tsx
@@ -0,0 +1,28 @@
+import { Flex, Image } from "antd"
+import styles from "./QuizHost.module.css"
+import jabuka from './apple_temp.png'
+import { useState, useEffect } from "react"
+
+
+export default function QuizHost(){
+
+    const [rotation, setRotation] = useState(0);
+
+    useEffect(() => {
+        // Generate a random rotation between -10 and 10 degrees
+        const randomRotation = Math.floor(Math.random() * 21) - 10; 
+        setRotation(randomRotation);
+    }, []);
+
+
+
+    return(
+    <Flex className={styles.question}>
+
+        <Image className={styles.image} src={jabuka} preview={false} style={{ transform: `rotate(${rotation}deg)` }}> {/*TEMPORARY DOKLER NI POVEZANO*/}
+        </Image>
+        <h1 style={{margin: '0px', textAlign: 'center'}}>
+            ADD TEXT HERE
+        </h1>
+    </Flex>)
+}
