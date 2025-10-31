c031d7e3d035a3667f4625ba47d0c7589e9f1ab9
mzHap <mojcazamuda12@gmail.com>
2025-10-31 19:52:56 +0100

    Add floating info card with player stats to quiz page

--- src/pages/QuizAnswering/QuizAnswering.tsx
+++ src/pages/QuizAnswering/QuizAnswering.tsx
@@ -1,15 +1,13 @@
-import { Button, Flex, Form, Image } from "antd";
+import { Button, Flex, Form, Image, Card } from "antd";
 import jabuka from "./apple_temp.png";
 import jabuka2 from "./t1.png";
 import jabuka3 from "./t2.png";
 import jabuka4 from "./t3.png";
-import styles from "./QuizAnswering.module.css"
-
+import styles from "./QuizAnswering.module.css";
 import TextArea from "antd/es/input/TextArea";
 
-
 export default function QuizAnswering() {
-    const testSwitch = 1;
+    const testSwitch = 2;
 
     type Answer = {
         content: string;
@@ -28,7 +26,7 @@ export default function QuizAnswering() {
         {
             answerType: 2,
             answers: [
-                { content: "Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris Paris", isTrue: true },
+                { content: "Paris", isTrue: true },
                 { content: "London", isTrue: false },
                 { content: "Berlin", isTrue: false },
                 { content: "Rome", isTrue: false },
@@ -45,87 +43,98 @@ export default function QuizAnswering() {
         },
     ];
 
-    const colorClasses = [
-        styles.blue,
-        styles.pink,
-        styles.orange,
-        styles.green,
-    ];
+    const colorClasses = [styles.blue, styles.pink, styles.orange, styles.green];
 
     return (
-        <>
-            <Flex style={{ width: "100%" }} vertical align="center" justify="center" className={styles.container}>
-                <h1 className={styles.header}>ODGOVORI:</h1>
-                {testing1[testSwitch].answerType === 1 && (
-                    <Form style={{ width: "80%" }}>
-                        <Form.Item
-                            name={["answer"]}
-                            rules={[{ required: true, message: "" }]}
-                        >
-                            <TextArea autoSize={{ minRows: 5, maxRows: 5 }} style={{ backgroundColor: "#fff", color: "#000", border: "15px solid #5FAFF5" }} className={styles.textField} />
-                        </Form.Item>
-                        <Button type="primary" htmlType="submit" className={styles.commitButton}>
-                            Oddaj
-                        </Button>
-                    </Form>
-                )}
+        <Flex
+            style={{ width: "100%", position: "relative" }}
+            vertical
+            align="center"
+            justify="center"
+            className={styles.container}
+        >
+            <Card className={styles.infoCard}>
+                <div className={styles.infoTitle}>Tvoja statistika</div>
+                <div>Mesto: #5</div>
+                <div>Točke: 1450 </div>
+                <div>+25 točk da prihitiš igralca #4</div>
+                <div>+80 točk pred igralcem #6</div>
+            </Card>
 
-                {testing1[testSwitch].answerType === 2 && (
-                    <Form>
-                        <Flex wrap="wrap" justify="center" gap="large">
-                            {testing1[testSwitch].answers?.map((answer, index) => (
-                                <Button
-                                    key={index}
-                                    type="default"
-                                    htmlType="button"
-                                    className={`${styles.textButton} ${colorClasses[index % colorClasses.length]}`}
-                                >
-                                    <div className={styles.textButtonContent}>
-                                        {answer.content}
-                                    </div>
-                                </Button>
-                            ))}
-                        </Flex>
-                    </Form>
-                )}
+            <h1 className={styles.header}>ODGOVORI:</h1>
 
-                {testing1[testSwitch].answerType === 3 && (
-                    <Form>
-                        <Flex
-                            wrap="wrap"
-                            justify="center"
-                            align="center"
-                            gap="large"
-                            style={{ width: "100%" }}
-                        >
-                            {testing1[testSwitch].answers?.map((answer, index) => (
+            {testing1[testSwitch].answerType === 1 && (
+                <Form style={{ width: "80%" }}>
+                    <Form.Item name={["answer"]} rules={[{ required: true, message: "" }]}>
+                        <TextArea
+                            autoSize={{ minRows: 5, maxRows: 5 }}
+                            style={{
+                                backgroundColor: "#fff",
+                                color: "#000",
+                                border: "15px solid #5FAFF5",
+                            }}
+                            className={styles.textField}
+                        />
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit" className={styles.commitButton}>
+                        Oddaj
+                    </Button>
+                </Form>
+            )}
 
+            {testing1[testSwitch].answerType === 2 && (
+                <Form>
+                    <Flex wrap="wrap" justify="center" gap="large">
+                        {testing1[testSwitch].answers?.map((answer, index) => (
+                            <Button
+                                key={index}
+                                type="default"
+                                htmlType="button"
+                                className={`${styles.textButton} ${
+                                    colorClasses[index % colorClasses.length]
+                                }`}
+                            >
+                                <div className={styles.textButtonContent}>{answer.content}</div>
+                            </Button>
+                        ))}
+                    </Flex>
+                </Form>
+            )}
 
-                                <Button
-                                    key={index}
-                                    type="default"
-                                    htmlType="button"
-                                    className={`${styles.imageButton} ${colorClasses[index % colorClasses.length]}`}
-                                >
-                                    <div className={styles.imageWrapper}>
-                                        <Image
-                                            src={answer.content}
-                                            preview={false}
-                                            style={{
-                                                width: "100%",
-                                                height: "100%",
-                                                objectFit: "contain",
-                                            }}
-                                        />
-                                    </div>
-                                </Button>
-                            ))}
-                        </Flex>
-                    </Form>
-                )}
-
-
-            </Flex>
-        </>
+            {testing1[testSwitch].answerType === 3 && (
+                <Form>
+                    <Flex
+                        wrap="wrap"
+                        justify="center"
+                        align="center"
+                        gap="large"
+                        style={{ width: "100%" }}
+                    >
+                        {testing1[testSwitch].answers?.map((answer, index) => (
+                            <Button
+                                key={index}
+                                type="default"
+                                htmlType="button"
+                                className={`${styles.imageButton} ${
+                                    colorClasses[index % colorClasses.length]
+                                }`}
+                            >
+                                <div className={styles.imageWrapper}>
+                                    <Image
+                                        src={answer.content}
+                                        preview={false}
+                                        style={{
+                                            width: "100%",
+                                            height: "100%",
+                                            objectFit: "contain",
+                                        }}
+                                    />
+                                </div>
+                            </Button>
+                        ))}
+                    </Flex>
+                </Form>
+            )}
+        </Flex>
     );
 }
